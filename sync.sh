06c5b8c51e7e62cf50adaa3d56af77dd3205775b
David Key <davidkey@gmail.com>
2013-09-02 22:55:05 -0400

    Fixed UserInterface

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
-rsync -av --exclude='app/storage' --exclude='vendor' . root@192.168.1.133:/var/likemycat_laravel/
-rsync -av --exclude='app/storage' root@192.168.1.133:/var/likemycat_laravel/ .
+rsync -av --exclude '.git' --exclude='app/storage' --exclude='vendor' . root@192.168.1.133:/var/likemycat_laravel/
+rsync -av --exclude '.git' --exclude='app/storage' root@192.168.1.133:/var/likemycat_laravel/ .
