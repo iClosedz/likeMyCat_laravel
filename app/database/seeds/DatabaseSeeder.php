30b80868683d53a6c1fddd7bec1124e4943947f3
David Key <davidkey@gmail.com>
2013-09-06 18:14:35 -0400

    Added db seeding for initial deployment.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,31 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
 	}
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        // create admin user
+        User::create(array(
+        	'email' => 'admin@admin.com',
+        	'password' => Hash::make('admin'),
+        	'is_guest' => false,
+        	'ip_address' => ip2long(isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : 1)
+        	));
+
+        // create guest user
+         User::create(array(
+        	'email' => 'guest@guest.com',
+        	'password' => 'NOT_A_PASSWORD', // guest shouldn't be login-able
+        	'is_guest' => true,
+        	'ip_address' => ip2long(isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : 1)
+        	));
+    }
 
 }
