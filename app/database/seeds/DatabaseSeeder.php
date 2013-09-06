b9b69c9d5efdbb3f75d2907d7ccde16841e8a7d0
David Key <davidkey@gmail.com>
2013-09-06 18:35:35 -0400

    Update seeding data with default roles.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,31 +11,63 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
+		$this->call('RoleTableSeeder');
 		$this->call('UserTableSeeder');
+	}
+}
+
+class RoleTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('roles')->delete();
+
+        // create roles
+		Role::create(array('admin'));
+		Role::create(array('user'));
+		Role::create(array('uploader'));
+		Role::create(array('reviewer'));
+		Role::create(array('guest'));
 	}
 }
 
 class UserTableSeeder extends Seeder {
 
-    public function run()
-    {
-        DB::table('users')->delete();
+	public function run()
+	{
+		DB::table('users')->delete();
 
         // create admin user
-        User::create(array(
-        	'email' => 'admin@admin.com',
-        	'password' => Hash::make('admin'),
-        	'is_guest' => false,
-        	'ip_address' => ip2long(isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : 1)
-        	));
+		User::create(array(
+			'email' => 'admin@admin.com',
+			'password' => Hash::make('admin'),
+			'is_guest' => false,
+			'ip_address' => ip2long(isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : 1)
+			));
 
         // create guest user
-         User::create(array(
-        	'email' => 'guest@guest.com',
+		User::create(array(
+			'email' => 'guest@guest.com',
         	'password' => 'NOT_A_PASSWORD', // guest shouldn't be login-able
         	'is_guest' => true,
         	'ip_address' => ip2long(isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : 1)
         	));
-    }
 
+		$adminRole = Role::getByRoleName('admin');
+		$userRole = Role::getByRoleName('user');
+		$uploaderRole = Role::getByRoleName('uploader');
+		$reviewerRole = Role::getByRoleName('reviewer');
+		$guestRole = Role::getByRoleName('guest');
+
+		$adminUser = User::getUserByEmail('admin@admin.com');
+		$guestUser = User::getUserByEmail('guest@guest.com');
+
+		$adminUser->grantRole($adminRole)
+					->grantRole($userRole)
+					->grantRole($uploaderRole)
+					->grantRole($reviewerRole);
+					
+		$guestUser->grantRole($guestRole);
+	}
 }
+
