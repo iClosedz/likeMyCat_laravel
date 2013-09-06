434e12b08c866c88f40692353bb5079811e52e56
David Key <davidkey@gmail.com>
2013-09-06 17:47:29 -0400

    test commit

--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 rsync -av --exclude 'uploads' --exclude '.git' --exclude='app/storage' --exclude='vendor' . root@192.168.1.133:/var/likemycat_laravel/
 rsync -av --exclude 'uploads' --exclude '.git' --exclude='app/storage' root@192.168.1.133:/var/likemycat_laravel/ .
+echo 'sync complete'
