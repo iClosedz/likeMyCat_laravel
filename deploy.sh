0599f02d0143cefd9b358baa4a71ee4d5295fd61
David Key <davidkey@gmail.com>
2013-09-07 22:35:53 -0400

    Updated deployment files.

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,12 @@ echo 'grabbing latest code from github'
 time git fetch --all
 time git reset --hard origin/master
 
+echo 'updating permissins'
+chown -R :www-data .
+chmod -R 775 uploads
+chmod -R 775 app/storage
+
 echo 'composer install'
 composer install
+
+echo 'done'
