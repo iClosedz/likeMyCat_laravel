e96a8485c052a53a53a20df6bddbc32ae176998d
David Key <davidkey@gmail.com>
2013-09-07 22:44:30 -0400

    Fixed formatting on signup page.

--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -4,7 +4,41 @@
 	<title>Sign Up</title>
 @stop
 
+@section('customStyles')
+	body {
+		padding-top: 40px;
+		padding-bottom: 40px;
+		background-color: #f5f5f5;
+	}
+
+	.form-signin {
+		max-width: 300px;
+		padding: 19px 29px 29px;
+		margin: 0 auto 20px;
+		background-color: #fff;
+		border: 1px solid #e5e5e5;
+		-webkit-border-radius: 5px;
+		-moz-border-radius: 5px;
+		border-radius: 5px;
+		-webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, .05);
+		-moz-box-shadow: 0 1px 2px rgba(0, 0, 0, .05);
+		box-shadow: 0 1px 2px rgba(0, 0, 0, .05);
+	}
+
+	.form-signin .form-signin-heading,.form-signin .checkbox {
+		margin-bottom: 10px;
+	}
+
+	.form-signin input[type="text"],.form-signin input[type="password"] {
+		font-size: 16px;
+		height: auto;
+		margin-bottom: 15px;
+		padding: 7px 9px;
+	}
+@stop
+
 @section('content')
+<div>
 	{{ Form::open(array('route' => 'post signup', 'class'=>'form-signin')) }}
 		<h2 class="form-signin-heading">New User</h2>
 		{{ Form::email('username', null, array('class' => 'input-block-level', 'placeholder' => 'Email')) }}
@@ -12,4 +46,5 @@
 		{{ Form::password('password', array('class' => 'input-block-level', 'placeholder' => 'Confirm Password')) }}
 		{{ Form::button('Sign Up', array('class' => 'btn btn-large btn-primary', 'type' => 'submit')) }}
 	{{ Form::close() }}
+</div>
 @stop
