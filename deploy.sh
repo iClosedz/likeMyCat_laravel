b4238a766b2c2a3bddef19126d19360b6b9032ed
David Key <davidkey@gmail.com>
2013-09-09 09:00:08 -0400

    Put site in maintenance mode when deployment is processing.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-## see http://stackoverflow.com/questions/1125968/force-git-to-overwrite-local-files-on-pull
+echo 'Put site in maintenance mode'
+php artisan down
 
+## see http://stackoverflow.com/questions/1125968/force-git-to-overwrite-local-files-on-pull
 echo 'grabbing latest code from github'
 time git fetch --all
 time git reset --hard origin/master
@@ -9,12 +11,15 @@ time git reset --hard origin/master
 echo 'updating permissins'
 chown -R :www-data .
 chmod -R 775 uploads
-chmod -R 775 app/storage
+chmod -R 775 app/storage 2>/dev/null
 
 echo 'composer install'
 composer install
 
 echo 'migrate database changes'
 php artisan migrate
+
+echo 'Bring site back up'
+php artisan up
 
 echo 'done'
