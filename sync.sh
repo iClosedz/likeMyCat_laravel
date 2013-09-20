f2c4ef85eba0fcb00f7140576deb93dcbe510de4
David Key <davidkey@gmail.com>
2013-09-20 15:34:00 -0500

    Updated /vendor…again

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
-rsync -av --exclude 'composer.json' --exclude 'composer.lock' --exclude '/uploads' --exclude '.git' --exclude='app/storage' --exclude='vendor' . root@192.168.0.113:/var/likemycat_laravel/
+rsync -av --exclude 'composer.json' --exclude 'composer.lock' --exclude '/uploads' --exclude '.git' --exclude='app/storage' . root@192.168.0.113:/var/likemycat_laravel/
 rsync -av --exclude '/uploads' --exclude '.git' --exclude='app/storage' root@192.168.0.113:/var/likemycat_laravel/ .
 echo 'sync complete'
